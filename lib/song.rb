ba9bd875c00bf42e6b3b88d703df01614d8567d6
Gitanjali Mule <gitanjalidmule@gmail.com>
2019-03-27 03:08:19 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,11 +9,16 @@ class Song
     @name = name
   end
 
+<<<<<<< HEAD
   def artist_name= (name)
+=======
+  def artist= (name)
+>>>>>>> 743f50e6f1e50ae3c434a76bef79e73b8cdc7788
     self.artist = Artist.find_or_create_by_name(name)
     artist.add_song(self)
   end
 
+<<<<<<< HEAD
   def self.new_by_filename(file)
     song_info = file.chomp(".mp3").split(" - ")
     song = Song.new(song_info[1])
@@ -22,4 +27,6 @@ class Song
 
   end
 
+=======
+>>>>>>> 743f50e6f1e50ae3c434a76bef79e73b8cdc7788
 end
